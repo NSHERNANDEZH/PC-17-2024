607460853ea52e694e5b233971449b35d3dc5005
NSHERNANDEZH <157499736+NSHERNANDEZH@users.noreply.github.com>
2024-02-27 10:02:52 -0600

    Add files via upload

---
+++ semana7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class PROGRAM
+ 
+    {
+        static void Main(string[] args) 
+        {
+
+
+         Console.WriteLine("Mi segundo programa");
+
+
+        Console.WriteLine("Ingrese su nombre");
+          string sNombre = Console.ReadLine();
+
+          Console.WriteLine("Ingrese su edad");
+          string sEdad = Console.ReadLine();
+
+          Console.WriteLine("Ingrese su carrera");
+          string sCarrera = Console.ReadLine();
+
+          Console.WriteLine("Ingrese su carne");
+          string sCarne = Console.ReadLine();
+
+          Console.WriteLine("Soy " + sNombre + " tengo " + sEdad + " años y estudio la carrera de " + sCarrera +
+          " Mi número de carné es " + sCarne);
+
+          Console.ReadKey();
+        }
+    }
+
+
+    
+ 
+
+ 
